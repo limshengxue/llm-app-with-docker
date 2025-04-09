a4c456a260ade11ed989ad001832b5f80de5ca97
LIM SHENG XUE <limshengxue0820@gmail.com>
2025-04-09 13:56:16 +0800

    LLMDOC 3 - LangChain Integeration

--- api/app.py
+++ api/app.py
@@ -1,22 +1,35 @@
-import requests
 import os
 from fastapi import FastAPI, Response
+from langchain_ollama import ChatOllama
+from pydantic import BaseModel
 
 app = FastAPI()
 
 ollama_url = os.environ.get('OLLAMA_URL', 'http://ollama:11434')
 model_name = os.environ.get('MODEL_NAME', 'gemma3:1b')
 
+class QueryInput(BaseModel):
+    query: str
+
 @app.get('/')
 def home():
     return {"Chat" : "Bot"}
 
-@app.get('/ask')
-def ask(prompt :str):
-    res = requests.post(ollama_url + '/api/generate', json={
-        "prompt": prompt,
-        "stream" : False,
-        "model" : model_name
-    })
+@app.post('/query')
+def query(input : QueryInput):
+    llm = ChatOllama(
+        base_url=ollama_url,
+        model=model_name,
+    )
 
-    return Response(content=res.text, media_type="application/json")
+    messages = [
+        (
+            "system",
+            "Think carefully and answer the question as concisely as possible.",
+        ),
+        ("human", input.query),
+    ]
+
+    ai_msg = llm.invoke(messages)
+    
+    return {"response" : ai_msg.content}
